c8f5fc54d160e9e35a51cebd9e99895c4b5df042
41issonm <164199364+41issonm@users.noreply.github.com>
2024-04-07 16:58:01 -0300

    feat86azvat8w:tela de rastreio
    Criacao da tela de rastreio de trilhas e adiciao dos enpoints de status e redirecionamento

--- api/src/views/rastreio/RastreioView.vue
+++ api/src/views/rastreio/RastreioView.vue
@@ -1,54 +1,46 @@
 <template>
-  <select id="" style="
-      margin-right: 700px;
-      width: 200px; 
-  ">
-    <!-- <option value="" disabled selected hidden></option> -->
-    <option value="colaborador1">Colaborador 1</option>
-    <option value="colaborador2">Colaborador 2</option>
-  </select>
+  <div>
+    <h2 style="padding-right: 800px;">Rastreio</h2>
 
-  <div class="container">
-    <h2>Tabela com Três Colunas</h2>
+    <div class="container">
+      <select class="select">
+        <option value="colaborador1">Parceiros </option>
+        <option value="colaborador2">Colaborador 2</option>
+      </select>
 
-    <table style="
-    margin-right: 30px;
-    margin-left: 450px;
-">
-      <thead>
-        <tr>
-          <th>Parceiro</th>
-          <th>Status</th>
-          <th>Ação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{{ nome }}</td>
-          <td>
-            <button @click="abrirModal">status</button>
-          </td>
-          <td>
-            <router-link to="/trilhas">Ação</router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
-  <!-- Modal -->
-  <div v-show="modalAberto" class="modal">
-    <div class="modal-content">
-      <span class="fechar" @click="fecharModal">&times;</span>
-      <p>Modal de status</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Parceiro</th>
+            <th>Status</th>
+            <th>Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="nome in nomeColaborador" :key="nome" value="id">
+            <td>{{ nome }}</td>
+            <td>
+              <button type="button" class="btn btn-secondary" @click="abrirModal">Status</button>
+            </td>
+            <td>
+              <router-link to="/trilhas">
+                <button type="button" class="btn btn-light">Acompanhar trilhas</button>
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
   </div>
 </template>
 
 <script lang="ts">
+import axios from "axios"
+import Swal from 'sweetalert2';
+
 import { Options, Vue } from "vue-class-component";
 import { RouterLink } from "vue-router";
-import axios from "axios";
 @Options({
   name: "RastreioView",
   components: {
@@ -56,24 +48,71 @@ import axios from "axios";
   }
 })
 export default class RastreioView extends Vue {
-  nome: string = "";
+  nomeColaborador: string[] = [];
   status: boolean = false;
   modalAberto: boolean = false;
+  acompanhamentoTrilha: { nomeDatrilha: string, porcentagemEmanadamento: string }[] = [];
 
   mounted() {
     this.getNomeParceiro();
   }
 
-  getNomeParceiro() {
-    axios.get<{ nome: string; id: number }>('/colaborador/1')
-      .then(x => {
-        console.log(x)
-      });
-  }
+  async getNomeParceiro() {
+    axios.get('/colaborador/1')
+        .then((response) => {
+            console.log(response)
+            for (let i = 0; i < response.data.length; i++) {
+                this.nomeColaborador.push(response.data[i].nome);
+                console.log(response.data[i].nome);
+            }
+        });
+    }
 
   abrirModal() {
-    this.modalAberto = true;
-  }
+  this.modalAberto = true;
+
+  axios.get('/progresso-colaborador/1')
+    .then((response) => {
+      this.acompanhamentoTrilha = [];
+      this.acompanhamentoTrilha = response.data.map((item: any) => ({
+        nomeDatrilha: item.nomeTrilha,
+        porcentagemEmanadamento: item.porcentagemAndamento
+      }));
+
+      if (this.acompanhamentoTrilha.length > 0) {
+        this.acompanhamentoTrilha.forEach((item) => {
+          Swal.fire({
+            title: 'Status',
+            html: `
+              <p>Nome da trilha: ${item.nomeDatrilha}</p>
+              <p>Progresso do colaborador: ${item.porcentagemEmanadamento}</p>
+            `,
+            showCloseButton: true,
+            showConfirmButton: false,
+          });
+        });
+      } else {
+        Swal.fire({
+          title: 'Aviso',
+          text: 'Nenhum dado disponível para exibir',
+          icon: 'warning',
+          showCloseButton: true,
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      Swal.fire({
+        title: 'Erro',
+        text: 'Erro ao obter dados da trilha',
+        icon: 'error',
+        showCloseButton: true,
+      });
+    });
+}
+
+
+
 
   fecharModal() {
     this.modalAberto = false;
@@ -82,42 +121,43 @@ export default class RastreioView extends Vue {
 </script>
 
 <style>
+table{
+  padding-bottom: 200px;
+}
+th {
+  padding-right: 100px; /* Adiciona 20px de espaçamento à direita de cada coluna */
+}
+td{
+  padding-right: 100px;
+  padding-bottom: 20px;
+}
+
+select {
+  position: absolute;
+  width: 110px;
+  height: 35px;
+  top: 210px; 
+  left: 310px;
+  opacity: 1;
+  background-color: #bfb8b8;
+  border-radius: 10px;
+}
+
+
+
+
 .container {
-  background-color: white;
-  padding: 20px;
-  border-radius: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 1099px;
+  height: 380px;
+  margin: auto;
+  border-radius: 20px;
+  background-color: #EDEDED;
+  opacity: 1; /* Alterei para 1 para garantir que o container seja visível */
 }
 
-/* Estilos para o modal */
-.modal {
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-}
 
-.modal-content {
-  background-color: #fefefe;
-  margin: 15% auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-.fechar {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.fechar:hover,
-.fechar:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
 </style>
