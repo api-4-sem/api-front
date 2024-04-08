086c3a909ae9683e9ecd0dc37101d6716aa72423
Matheus Fiebig <macrubig@gmail.com>
2024-04-08 19:33:54 -0300

    feat 86azvat8w solicitar feedback

--- api/src/views/rastreio/RastreioView.vue
+++ api/src/views/rastreio/RastreioView.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <h2 style="padding-right: 800px;">Rastreio</h2>
+    <h2 style="padding-right: 800px">Rastreio</h2>
 
     <div class="container">
       <select class="select">
-        <option value="colaborador1">Parceiros </option>
+        <option value="colaborador1">Parceiros</option>
         <option value="colaborador2">Colaborador 2</option>
       </select>
 
@@ -17,102 +17,117 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="nome in nomeColaborador" :key="nome" value="id">
-            <td>{{ nome }}</td>
+          <tr v-for="nome in nomeColaborador" :key="nome.nome" value="id">
+            <td>{{ nome.nome }}</td>
             <td>
-              <button type="button" class="btn btn-secondary" @click="abrirModal">Status</button>
+              <button type="button" class="btn btn-secondary" @click="abrirModal">
+                Status
+              </button>
             </td>
             <td>
               <router-link to="/trilhas">
-                <button type="button" class="btn btn-light">Acompanhar trilhas</button>
+                <button type="button" class="btn btn-light">
+                  Acompanhar trilhas
+                </button>
               </router-link>
+              <button type="button" :on-click="solicitarFeedback(nome.id)" class="btn btn-info">
+                Solicitar Feedback
+              </button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-
   </div>
 </template>
 
 <script lang="ts">
-import axios from "axios"
-import Swal from 'sweetalert2';
+import axios from "axios";
+import Swal from "sweetalert2";
 
 import { Options, Vue } from "vue-class-component";
 import { RouterLink } from "vue-router";
 @Options({
   name: "RastreioView",
   components: {
-    RouterLink
-  }
+    RouterLink,
+  },
 })
 export default class RastreioView extends Vue {
-  nomeColaborador: string[] = [];
+  nomeColaborador: {
+    id: number,
+    nome: string
+  }[] = [];
   status: boolean = false;
   modalAberto: boolean = false;
-  acompanhamentoTrilha: { nomeDatrilha: string, porcentagemEmanadamento: string }[] = [];
+  acompanhamentoTrilha: {
+    nomeDatrilha: string;
+    porcentagemEmanadamento: string;
+  }[] = [];
 
   mounted() {
     this.getNomeParceiro();
   }
 
   async getNomeParceiro() {
-    axios.get('/colaborador/1')
-        .then((response) => {
-            console.log(response)
-            for (let i = 0; i < response.data.length; i++) {
-                this.nomeColaborador.push(response.data[i].nome);
-                console.log(response.data[i].nome);
-            }
-        });
-    }
+    axios.get("/colaborador/1").then((response) => {
+      this.nomeColaborador = response.data;
+    });
+  }
 
   abrirModal() {
-  this.modalAberto = true;
+    this.modalAberto = true;
 
-  axios.get('/progresso-colaborador/1')
-    .then((response) => {
-      this.acompanhamentoTrilha = [];
-      this.acompanhamentoTrilha = response.data.map((item: any) => ({
-        nomeDatrilha: item.nomeTrilha,
-        porcentagemEmanadamento: item.porcentagemAndamento
-      }));
+    axios
+      .get("/progresso-colaborador/1")
+      .then((response) => {
+        this.acompanhamentoTrilha = [];
+        this.acompanhamentoTrilha = response.data.map((item: any) => ({
+          nomeDatrilha: item.nomeTrilha,
+          porcentagemEmanadamento: item.porcentagemAndamento,
+        }));
 
-      if (this.acompanhamentoTrilha.length > 0) {
-        this.acompanhamentoTrilha.forEach((item) => {
-          Swal.fire({
-            title: 'Status',
-            html: `
+        if (this.acompanhamentoTrilha.length > 0) {
+          this.acompanhamentoTrilha.forEach((item) => {
+            Swal.fire({
+              title: "Status",
+              html: `
               <p>Nome da trilha: ${item.nomeDatrilha}</p>
               <p>Progresso do colaborador: ${item.porcentagemEmanadamento}</p>
             `,
-            showCloseButton: true,
-            showConfirmButton: false,
+              showCloseButton: true,
+              showConfirmButton: false,
+            });
           });
-        });
-      } else {
+        } else {
+          Swal.fire({
+            title: "Aviso",
+            text: "Nenhum dado disponível para exibir",
+            icon: "warning",
+            showCloseButton: true,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
         Swal.fire({
-          title: 'Aviso',
-          text: 'Nenhum dado disponível para exibir',
-          icon: 'warning',
+          title: "Erro",
+          text: "Erro ao obter dados da trilha",
+          icon: "error",
           showCloseButton: true,
         });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
+      });
+  }
+
+  solicitarFeedback(colaboradorId: number) {
+    axios.post("feedback/" + colaboradorId).then((r) => {
       Swal.fire({
-        title: 'Erro',
-        text: 'Erro ao obter dados da trilha',
-        icon: 'error',
+        html: `Feedback solicitado ao parceiro`,
         showCloseButton: true,
+        showConfirmButton: false,
       });
     });
-}
-
-
-
+  }
 
   fecharModal() {
     this.modalAberto = false;
@@ -121,13 +136,15 @@ export default class RastreioView extends Vue {
 </script>
 
 <style>
-table{
+table {
   padding-bottom: 200px;
 }
+
 th {
-  padding-right: 100px; 
+  padding-right: 100px;
 }
-td{
+
+td {
   padding-right: 100px;
   padding-bottom: 20px;
 }
@@ -136,15 +153,12 @@ select {
   position: absolute;
   width: 110px;
   height: 35px;
-  top: 210px; 
+  top: 210px;
   left: 310px;
   opacity: 1;
   background-color: #bfb8b8;
   border-radius: 10px;
 }
-
-
-
 
 .container {
   display: flex;
@@ -155,9 +169,7 @@ select {
   height: 380px;
   margin: auto;
   border-radius: 20px;
-  background-color: #EDEDED;
-  opacity: 1; 
+  background-color: #ededed;
+  opacity: 1;
 }
-
-
 </style>
