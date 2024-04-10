6cc3efc3300f1f759c2f5fb8d8725676638cb9c3
Jhonny Freitas Dutra <dutrajy@gmail.com>
2024-04-10 20:44:51 -0300

    feat: Obtendo informações de visualização da api

--- api/src/views/visualizacao/VisualizacaoView.vue
+++ api/src/views/visualizacao/VisualizacaoView.vue
@@ -2,28 +2,26 @@
     <div>
         <h2 style="width: 1099px; margin: 1rem auto; text-align: left;">Visualização de Dados</h2>
         <div class="container">
-            <div class="select-row">
-                <select class="select">
-                    <option>Selecione Parceiro...</option>
-                    <option value="parceiro1">Parceiro 1</option>
-                    <option value="parceiro2">Parceiro 2</option>
-                    <option value="parceiro3">Parceiro 3</option>
-                </select>
-            </div>
+            <select class="select" @change="getProgresso($event.target?.value)">
+                <option :value="0">Selecione Parceiro...</option>
+                <option v-for="colaborador in listaDeColaboradores" :key="colaborador.id" :value="colaborador.id">
+                    {{ colaborador.nome }} #{{ colaborador.id }}
+                </option>
+            </select>
             <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
         </div>
     </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
 import { Options, Vue } from "vue-class-component";
 import { RouterLink } from "vue-router";
 
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 @Options({
     name: "VisualizacaoView",
@@ -33,33 +31,59 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
     },
 })
 export default class VisualizacaoView extends Vue {
-    data() {
-        return {
-            chartData: {
-                labels: ['Application Integration', 'Big Data', 'Data Lake', 'Data Science'],
-                datasets: [{ data: [100, 70, 40, 20] }]
-            },
-            chartOptions: {
-                responsive: true,
-                indexAxis: 'y',
-            }
+    listaDeColaboradores: {
+        id: number,
+        nome: string
+    }[] = [];
+    chartData: any = {
+        labels: [],
+        datasets: [{ data: [] }]
+    };
+    chartOptions = {
+        responsive: true,
+        indexAxis: 'y',
+    };
+
+    mounted() {
+        this.getColaboradores();
+    }
+
+    async getColaboradores() {
+        const response = await axios.get("colaborador/1");
+        this.listaDeColaboradores = response.data;
+    }
+
+    async getProgresso(colaboradorId: string | null) {
+        if (colaboradorId === null || colaboradorId === "0") {
+            this.chartData = {
+                labels: [],
+                datasets: [{ data: [] }]
+            };
+            return;
         }
+
+        const response = await axios.get(`progresso-colaborador/${colaboradorId}`);
+        const progresso = response.data as {
+            porcentagemAndamento: number,
+            nomeTrilha: string,
+        }[];
+
+        this.chartData = {
+            labels: progresso.map(p => p.nomeTrilha),
+            datasets: [{ data: progresso.map(p => p.porcentagemAndamento) }]
+        };
     }
 }
 </script>
 
 <style>
-.select-row {
-    display: flex;
-    align-items: start;
-}
-
 select {
     opacity: 1;
     background-color: #bfb8b8;
     border-radius: 8px;
     padding: 0.5rem;
     padding-right: 2rem;
+    width: auto;
     font-size: 1rem;
 }
 
