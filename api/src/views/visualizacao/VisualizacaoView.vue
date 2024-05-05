dba822d9e316f638abb69ec28fc4d40d3d3b7f43
beatricelopes <beatricelopes@outlook.com>
2024-05-04 21:00:23 -0300

    feat: botão para extração de xlsx

--- api/src/views/visualizacao/VisualizacaoView.vue
+++ api/src/views/visualizacao/VisualizacaoView.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <h2 style="width: 1099px; margin: 1rem auto; text-align: left;">Visualização de Dados</h2>
-        <div class="container">
-            <select class="select" id="select_visu" @change="getProgresso($event.target?.value)">
-                <option :value="0">Selecione Parceiro...</option>
-                <option v-for="colaborador in listaDeColaboradores" :key="colaborador.id" :value="colaborador.id">
-                    {{ colaborador.nome }} #{{ colaborador.id }}
-                </option>
-            </select>
-            <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
-        </div>
+  <div class="visualizacao__container">
+    <div class="header">
+      <h2>Visualização de Dados</h2>
+      <div class="buttons">
+        <select
+          class="select"
+          id="select_visu"
+          @change="getProgresso($event.target?.value)"
+        >
+          <option :value="0">Selecione Parceiro...</option>
+          <option
+            v-for="colaborador in listaDeColaboradores"
+            :key="colaborador.id"
+            :value="colaborador.id"
+          >
+            {{ colaborador.nome }} #{{ colaborador.id }}
+          </option>
+        </select>
+        <button class="btn btn-outline-success" v-on:click="extrairRelatorio()">Extrair Dados</button>
+      </div>
     </div>
+    <div class="card">
+      <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,105 +31,146 @@ import axios from "axios";
 import { Options, Vue } from "vue-class-component";
 import { RouterLink } from "vue-router";
 
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
 
 ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale);
 
 @Options({
-    name: "VisualizacaoView",
-    components: {
-        RouterLink,
-        Bar
-    },
+  name: "VisualizacaoView",
+  components: {
+    RouterLink,
+    Bar,
+  },
 })
 export default class VisualizacaoView extends Vue {
-    listaDeColaboradores: {
-        id: number,
-        nome: string
-    }[] = [];
-    chartData: any = {
-        labels: [],
-        datasets: [{ data: [] }]
-    };
-    chartOptions = {
-        backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-        borderColor: ['rgba(54, 162, 235)'],
-        responsive: true,
-        indexAxis: 'y',
-        scales: {
-            x: {
-                beginAtZero: true,
-                min: 0,
-                max: 100,
-                ticks: {
-                    callback: function(value: number) {
-                        return value + '%';
-                    }
-                }
-            }
-        }
-    };
+  listaDeColaboradores: {
+    id: number;
+    nome: string;
+  }[] = [];
+  chartData: any = {
+    labels: [],
+    datasets: [{ data: [] }],
+  };
+  chartOptions = {
+    backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+    borderColor: ["rgba(54, 162, 235)"],
+    responsive: true,
+    indexAxis: "y",
+    scales: {
+      x: {
+        beginAtZero: true,
+        min: 0,
+        max: 100,
+        ticks: {
+          callback: function (value: number) {
+            return value + "%";
+          },
+        },
+      },
+    },
+  };
 
-    mounted() {
-        this.getColaboradores();
-    }
+  mounted() {
+    this.getColaboradores();
+  }
 
-    async getColaboradores() {
-        const response = await axios.get("colaborador/1");
-        this.listaDeColaboradores = response.data;
-    }
-
-    async getProgresso(colaboradorId: string | null) {
-        if (colaboradorId === null || colaboradorId === "0") {
-            this.chartData = {
-                labels: [],
-                datasets: [{ data: [] }]
-            };
-            return;
-        }
-
-        const response = await axios.get(`progresso-colaborador/${colaboradorId}`);
-        const progresso = response.data as {
-            porcentagemAndamento: number,
-            nomeTrilha: string,
-        }[];
-
-        this.chartData = {
-            labels: progresso.map(p => p.nomeTrilha),
-            datasets: [{ data: progresso.map(p => p.porcentagemAndamento) }]
-        };
-    }
+  download(data:any){
+    var a = document.createElement("a") as any;
+    document.body.appendChild(a);
+    a.style = "display: none";
+    var blob =  new Blob([data], {
+                type: 'application/vnd.ms-excel'})
+        , url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.click();
+    window.URL.revokeObjectURL(url);
 }
+
+extrairRelatorio() {
+    axios.get("relatorio/1").then(x=>this.download(x.data));
+}
+
+  async getColaboradores() {
+    const response = await axios.get("colaborador/1");
+    this.listaDeColaboradores = response.data;
+  }
+
+  async getProgresso(colaboradorId: string | null | any) {
+    if (colaboradorId === null || colaboradorId === "0") {
+      this.chartData = {
+        labels: [],
+        datasets: [{ data: [] }],
+      };
+      return;
+    }
+
+    const response = await axios.get(`progresso-colaborador/${colaboradorId}`);
+    const progresso = response.data as {
+      porcentagemAndamento: number;
+      nomeTrilha: string;
+    }[];
+
+    this.chartData = {
+      labels: progresso.map((p) => p.nomeTrilha),
+      datasets: [{ data: progresso.map((p) => p.porcentagemAndamento) }],
+    };
+  }
+}
+
+
 </script>
 
 <style>
-#select_visu {
-    opacity: 1;
-    background-color: #D4D3D3;
-    border-radius: 8px;
-    padding: 0.5rem;
-    padding-right: 2rem;
+.visualizacao__container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-height: 30vh;
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    }
+
+  #select_visu {
+    border-radius: 4px;
+    padding: 0.6rem;
     width: 170px;
     font-size: 0.8rem;
-    position: absolute;
-    left: 10px; 
-    top: 9%; 
-    transform: translateY(-50%);
-}
+    margin-right: 10px;;
+  }
 
-.container {
+  .container_head {
     position: relative;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    align-items: baseline;
     width: 1099px;
-    min-height: 60vh;
     margin: auto;
     border-radius: 20px;
-    background-color: #ededed;
     opacity: 1;
-    gap: 2rem;
+    gap: 0.5rem;
     padding: 2rem 5rem;
+  }
+
+  .card {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    border-radius: 8px;
+    background-color: #ededed;
+    margin-bottom: 10px;
+    padding: 20px;
     box-shadow: 3px 5px 10px #555;
+  }
 }
 </style>
