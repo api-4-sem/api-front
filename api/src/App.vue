7c040085cb542a5a424291015ccf40b169e50b3d
Jhonny Freitas Dutra <dutrajy@gmail.com>
2024-05-26 17:57:01 -0300

    Adiciona mapa no menu

--- api/src/App.vue
+++ api/src/App.vue
@@ -12,6 +12,7 @@
         <router-link to="/avaliacao">Avaliação de Parceiro</router-link>
         <router-link to="/cadastroparceiros">Cadastro de Parceiros</router-link>
         <router-link to="/notificacoes">Configurações</router-link>
+        <router-link to="/por-estado-mapa">Parceiros por Estado</router-link>
 
       </div>
     </nav>
